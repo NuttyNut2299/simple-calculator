183e2c5c5a0561f3130a1e78fc43693afc1135ad
NuttyNut2299 <199890687+NuttyNut2299@users.noreply.github.com>
2025-07-22 16:00:00 +0800

    Add functions for decimal and toggle +/- buttons

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const display = document.querySelector("#display-container");
 const buttons = document.querySelectorAll("button").length;
 
+display.textContent = "0";
+
 let firstValue = 0;
 let secondValue = 0;
 let memoryValue = 0;
@@ -10,10 +12,19 @@ for (let i = 0; i < buttons ; i++) { // add event listner to all buttons
     document.querySelectorAll("button")[i].addEventListener("click", (e) => {
         const buttonPressed = e.target.textContent;
 
-        if (!isNaN(parseInt(buttonPressed))) {
+        // TODO: convert to switch cases if possible 
+        if (!isNaN(parseInt(buttonPressed))  && display.textContent !== "Error") {
             addNumberToDisplay(buttonPressed);
         } else if (buttonPressed === "CE" || buttonPressed === "AC" ) {
             clearDisplay(buttonPressed);
+        } else if (buttonPressed === "." && display.textContent !== "Error") {
+            addDecimal();
+        } else if (buttonPressed === "+/-" && display.textContent !== "Error") {
+            togglePositiveNegative();
+        } else if (buttonPressed === "%" && display.textContent !== "Error") {
+            convertToPercentage();
+        } else {
+            console.log("nothing");
         }
     });
 }
@@ -22,17 +33,63 @@ function addNumberToDisplay (numberButton) {
     const displayText = display.textContent;
     const displayValue = parseFloat(displayText);
     
-    if (displayValue === 0) {
-        display.textContent = numberButton;
-    } else if (displayText.length === 10) {
+    if (displayValue === 0 && !displayText.includes(".")) {
+        if (displayText.includes("-")) {
+            display.textContent = "-" + numberButton;
+        } else {
+            display.textContent = numberButton;
+        }
+    } else if (displayText.length >= 10) {
         display.textContent = "Error"; // Maximum 10 digits to avoid precision issues with floating point and int
-    } else if (displayText === 'Error') {
-        // do nothing
     } else {
         const newValue = displayText.concat(numberButton);
         display.textContent = newValue;
     }
 }
+
+function addDecimal () {  
+    const displayText = display.textContent;
+
+    if (displayText !== "Error") {
+        const newValue = display.textContent.concat(".");
+        if (newValue.length >= 10) {
+            display.textContent = "Error"; // Maximum 10 digits to avoid precision issues with floating point and int
+        } else {
+            display.textContent = newValue;
+            document.querySelector("#decimal-btn").disabled = true;
+        }
+    }
+
+}
+
+function togglePositiveNegative () {
+    const displayText = display.textContent;
+
+    if (displayText.includes("-") && displayText.length <= 10) {
+        display.textContent = displayText.slice(1);
+    } else if (displayText.length >= 10) {
+        display.textContent = "Error"; // Maximum 10 digits to avoid precision issues with floating point and int
+    } else {
+        const negativeValue = "-" + displayText;
+        display.textContent = negativeValue;
+    }
+}
+
+function convertToPercentage () {
+    // const displayText = display.textContent;
+    // const displayValue = parseFloat(displayText);
+
+    // const newValue = (displayValue / 100).toPrecision(2).toString();
+    // console.log(newValue);
+    // if (newValue.length >= 10) {
+    //     display.textContent = "Error"; // Maximum 10 digits to avoid precision issues with floating point and int
+    // } else {
+    //     display.textContent = newValue;
+    //     document.querySelector("#decimal-btn").disabled = true;
+    //     // document.querySelector("#percentage-btn").disabled = true;
+    // }
+}
+
 
 function clearDisplay (clearType) {
     if (clearType === "AC") {
@@ -41,4 +98,6 @@ function clearDisplay (clearType) {
         memoryValue = 0;
     }
     display.textContent = 0;
+    document.querySelector("#decimal-btn").disabled = false;
+    // document.querySelector("#percentage-btn").disabled = false;
 }
